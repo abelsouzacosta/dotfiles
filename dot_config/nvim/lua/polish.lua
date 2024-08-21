02822ee5ee18a2d05608c547fd75454ab1b946fa
abelsouzacosta <abelsouzacostajunior@proton.me>
2024-08-20 22:49:32 -0300

    [chore]: adding shortcuts to nvim

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -16,7 +16,9 @@ vim.filetype.add {
 }
 
 local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", opts) -- Map Tab to switch to the next buffer
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprevious<CR>", opts) -- Map Shift+Tab to switch to the previous buffer
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", opts) -- Map Ctrl+s to save file in normal mode
+vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>", opts) -- Map Ctrl+s to save file in insert mode
+vim.api.nvim_set_keymap("n", "<C-x>", ":bd<CR>", opts) -- Map Ctrl+x to close the current buffer
 
--- Set key bindings
-vim.api.nvim_set_keymap('n', '<Tab>', ':bnext<CR>', opts)       -- Map Tab to switch to the next buffer
-vim.api.nvim_set_keymap('n', '<S-Tab>', ':bprevious<CR>', opts) -- Map Shift+Tab to switch to the previous buffer
