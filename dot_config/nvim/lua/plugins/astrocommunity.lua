e86965e4f2cdc64af5a077f82589778829728e61
abelsouzacosta <abelsouzacostajunior@proton.me>
2025-05-24 22:12:38 -0300

    [chore]: adding new plugins for nvim

--- dot_config/nvim/lua/plugins/astrocommunity.lua
+++ dot_config/nvim/lua/plugins/astrocommunity.lua
@@ -5,4 +5,8 @@ return {
   { import = "astrocommunity.fuzzy-finder.fzf-lua" },
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.pack.full-dadbod" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.scrolling.cinnamon-nvim" },
+  { import = "astrocommunity.diagnostics.error-lens-nvim" },
+  { import = "astrocommunity.icon.mini-icons" },
 }
